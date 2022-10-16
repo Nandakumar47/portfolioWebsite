4bcc289bbd1978e106614a0581daca0cf23b68af
Nandakumar47 <nandakumar.p.r1999@gmail.com>
2022-10-16 23:06:24 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Pages/Home";
 function App() {
-  window.addEventListener("scroll", function () {
-    console.log(this.window.scrollY);
-  });
   return (
     <div className="app">
       <NavBar />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import { FiArrowRight } from "react-icons/fi";
+import axios from "axios";
 function Contact() {
+  const [message, setMessage] = useState({
+    name: "",
+    mobile: "",
+    email: "",
+    subject: "",
+    messageContent: "",
+  });
+  function inputInformations(e) {
+    setMessage({
+      ...message,
+      [e.target.name]: e.target.value,
+    });
+  }
+  function submitMessage(e) {
+    e.preventDefault();
+    axios
+      .post("https://salty-plains-73732.herokuapp.com/", message)
+      .then(function (response) {
+        console.log(response);
+      });
+    setMessage({
+      name: "",
+      mobile: "",
+      email: "",
+      subject: "",
+      messageContent: "",
+    });
+    console.log(message);
+  }
   return (
     <div className="contact__section">
       <div className="form__Div card">
         <form action="" className="contact__form ">
           <div>
             <p>YOUR NAME</p>
-            <input type="text" name="Name" />
+            <input
+              type="text"
+              value={message.name}
+              name="name"
+              onChange={inputInformations}
+            />
           </div>
           <div>
             <p>PHONE NUMBER</p>
-            <input type="text" name="Name" />
+            <input
+              type="tel"
+              name="mobile"
+              value={message.mobile}
+              onChange={inputInformations}
+            />
           </div>
           <div className="two__column">
             <p>EMAIL</p>
-            <input type="text" name="Name" />
+            <input
+              type="text"
+              name="email"
+              value={message.email}
+              onChange={inputInformations}
+            />
           </div>{" "}
           <div className="two__column">
             <p>SUBJECT</p>
-            <input type="text" name="Name" />
+            <input
+              type="text"
+              name="subject"
+              value={message.subject}
+              onChange={inputInformations}
+            />
           </div>{" "}
           <div className="two__column">
             <p>YOUR MESSAGE</p>
-            <textarea name="message" rows="5" />
+            <textarea
+              name="messageContent"
+              rows="5"
+              value={message.messageContent}
+              onChange={inputInformations}
+            />
           </div>
-          <button className="two__column card" style={{backgroundColor:"#ff014f"}}>
+          <button
+            className="two__column card"
+            style={{ backgroundColor: "#ff014f" }}
+            onClick={submitMessage}
+          >
             <div className="button__div">
-            <p>SEND MESSAGE</p>
-            <FiArrowRight />
+              <p>SEND MESSAGE</p>
+              <FiArrowRight />
             </div>
           </button>
         </form>
